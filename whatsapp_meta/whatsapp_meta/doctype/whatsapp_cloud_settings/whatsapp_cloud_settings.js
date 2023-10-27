e29bdb0729137e1a20a796b98a5e37283407719f
crazy-explore-r <developer.ragul@gmail.com>
2023-10-27 09:54:00 +0000

    feat: WhatsApp Settings

---
+++ whatsapp_meta/whatsapp_meta/doctype/whatsapp_cloud_settings/whatsapp_cloud_settings.js
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, Ragul KM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Whatsapp Cloud Settings', {
+	refresh: function (frm) {
+		if (frm.doc.access_token && frm.doc.phone_number_id) {
+			frm.add_custom_button('Verify Token', () => {
+				set_primary_action(frm);
+			}).addClass("btn-primary");
+		}
+	}
+});
+
+function set_primary_action(frm) {
+	frappe.prompt({
+		label: 'WhatsApp Number',
+		fieldname: 'phone_number',
+		fieldtype: 'Data',
+		description: 'WhatsApp number with country code(eg: 91)',
+		reqd: 1
+	}, (values) => {
+		send_verify_message(frm, values.phone_number)
+	})
+}
+
+function send_verify_message(frm, phone_number) {
+	frappe.call({
+		method: 'whatsapp_meta.whatsapp_meta.doctype.whatsapp_cloud_settings.whatsapp_cloud_settings.send_test_message',
+		args: {
+			"phone_number": phone_number
+		},
+		callback: function (r) {
+			if (r && r.message) {
+				frappe.msgprint({
+					title: __('Success'),
+					message: __("WhatsApp Account successfully configured. <br> Message ID : {0}", [r.message])
+				});
+			}
+		},
+		freeze: true,
+		freeze_message: ('Sending WhatsApp test Message.!!')
+	});
+}
---
+++ whatsapp_meta/whatsapp_meta/doctype/whatsapp_cloud_settings/whatsapp_cloud_settings.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2023, Ragul KM and contributors
+# For license information, please see license.txt
+
+import frappe
+import requests
+from frappe.model.document import Document
+
+class WhatsappCloudSettings(Document):
+	pass
+
+def get_access_token():
+	return frappe.utils.password.get_decrypted_password(
+		"WhatsApp Cloud Settings", "WhatsApp Cloud Settings", "access_token"
+	)
+
+@frappe.whitelist()
+def send_test_message(phone_number):
+	access_token = get_access_token()
+	api_base_url = "https://graph.facebook.com/v17.0"
+	phone_number_id = frappe.db.get_single_value("WhatsApp Cloud Settings", "phone_number_id")
+
+	endpoint = f"{api_base_url}/{phone_number_id}/messages"
+
+	response_data = {
+		"messaging_product": "whatsapp",
+		"recipient_type": "individual",
+		"to": phone_number,
+		"type": 'template',
+	}
+	response_data["template"] = {"name": 'hello_world', "language": { "code": "en_US" } }
+	response = requests.post(
+		endpoint,
+		json=response_data,
+		headers={
+			"Authorization": "Bearer " + access_token,
+			"Content-Type": "application/json",
+		},
+	)
+
+	if response.ok:
+		return response.json().get("messages")[0]["id"]
+	else:
+		frappe.throw(response.json().get("error").get("message"))
+
