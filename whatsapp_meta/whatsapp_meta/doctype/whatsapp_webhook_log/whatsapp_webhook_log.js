fb8044cc541090fe322f5834585d8e3d092df696
crazy-explore-r <developer.ragul@gmail.com>
2023-11-07 10:34:09 +0530

    New Doctypes created

---
+++ whatsapp_meta/whatsapp_meta/doctype/whatsapp_webhook_log/whatsapp_webhook_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ragul KM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WhatsApp Webhook Log', {
+	// refresh: function(frm) {
+
+	// }
+});
